8bf5218358afd393f89b268f796d6270614d64d0
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2024-01-30 00:37:50 -0300

    pegou todos os links de produtos

--- webscrap/scraper.py
+++ webscrap/scraper.py
@@ -19,21 +19,37 @@ for nextSibling in ultima_pagina.find_next_siblings():
     if nextSibling.name == 'li':
         uls.append(nextSibling.get_text().strip())
 
-#for item in uls:
- #   print(item.text.encode("utf-8"))
 ultima_pagina_index  = len(uls) - 2
 print(uls[ultima_pagina_index])
 
-lista_produtos = soup.find_all('li', class_='span3')
-nomes = lista_produtos[0].find('a', class_='nome-produto cor-secundaria').get_text().strip()
+
+#nomes = lista_produtos[0].find('a', class_='nome-produto cor-secundaria').get_text().strip()
 #nome = nomes.find('a', class_='nome-produto cor-secundaria').get_text().strip()
 
-links = lista_produtos[0].find_all("a", class_='produto-sobrepor') 
-for link in links:
-  print("Link:", link.get("href"), "Text:", link.string)
 
+# Pegar link de todos os produtos 
+lista_Links = []
+for i in range(1, int(uls[ultima_pagina_index])+1):
+    url_pagina = f'https://www.piscaled.com.br/eletronica?pagina={i}#'
+    site = requests.get(url_pagina, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    lista_produtos = soup.find_all('li', class_='span3')
+    index = 0
+    for prod in lista_produtos:
+        links = lista_produtos[index].find_all("a", class_='produto-sobrepor')
+        for link in links:
+          x = link.get("href")
+          lista_Links.append(x)
+        index = index + 1
+    
+for lin in lista_Links:
+    print(lin)
 
-print('=============')
+    
+#print(lista_produtos[4].find("a", class_='produto-sobrepor').get("href"), ' ', lista_produtos[4].find('a', class_='nome-produto cor-secundaria').get_text().strip())
 
-print(lista_produtos[4].find("a", class_='produto-sobrepor').get("href"), ' ', lista_produtos[4].find('a', class_='nome-produto cor-secundaria').get_text().strip())
+#nome = soup.find_all('a', class_='nome-produto cor-secundaria').get_text().strip()
 
+#links = lista_produtos[0].find_all("a", class_='produto-sobrepor') 
+#for link in links:
+#  print("Link:", link.get("href"), "Text:", link.string)
