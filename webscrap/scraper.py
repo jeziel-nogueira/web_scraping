8c61cbf56ff0eb403c3cf809fdd16946c9f868da
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2024-02-01 00:49:13 -0300

    salvando todos os dados e imagens

--- webscrap/scraper.py
+++ webscrap/scraper.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
@@ -21,6 +22,24 @@ for nextSibling in ultima_pagina.find_next_siblings():
 
 ultima_pagina_index  = len(uls) - 2
 
+def criar_pasta(nome):
+   pasta_main = 'produtos/'
+   if(not os.path.exists(pasta_main)):
+      os.mkdir(pasta_main)
+   if(not os.path.exists('produtos/{nome}/')):
+      try:
+         os.mkdir(os.path.join(pasta_main, nome))
+         img_path = 'produtos/' + nome
+         os.mkdir(os.path.join(img_path, 'images'))
+      except:
+         print('Folder alreade exist')
+
+def save_data(data_, prod_path):
+      path = ''
+      with open (prod_path, 'a', newline='', encoding='UTF-8') as f:
+       f.write(data_)
+ 
+   
 # Pegar link de todos os produtos 
 lista_Links = []
 for i in range(1, int(uls[ultima_pagina_index])+1):
@@ -37,7 +56,7 @@ for i in range(1, int(uls[ultima_pagina_index])+1):
         index = index + 1
 
 #percorrer lista de links de produtos e coletar dados
-index = 380
+index = 0
 for lin in lista_Links:
     url_pagina = lista_Links[index]
     site = requests.get(url_pagina, headers=headers)
@@ -49,6 +68,8 @@ for lin in lista_Links:
     prod_price = 0
     prod_estoque = 0
     
+    prod_folder = prod_code + '/'
+    criar_pasta(prod_folder)
     try:
        estoque = soup.find(class_='qtde_estoque').get_text().strip()
        prod_estoque = estoque
@@ -58,18 +79,25 @@ for lin in lista_Links:
        prod_price = 'nd'
        prod_estoque = 0
 
+    prd_data = prod_name + ';' + prod_code + ';' + prod_price + ';' + str(prod_estoque) + ';' + lista_Links[index] + '\n'
+    prod_path = 'produtos/' + prod_code + '/' + prod_code
+    save_data(prd_data, prod_path)
+
+    aux = 1
     for tag in soup():
       if 'data-imagem-grande' in tag.attrs:
         image_url = tag.get("data-imagem-grande")
         #img.save('/absolute/path/to/myphoto.jpg', 'JPEG')
         img = Image.open(requests.get(image_url, stream = True).raw)
         img_name = tag.get("title") + '.jpg'
-        img.save(img_name, 'JPEG')
+        img_path = 'produtos/' + prod_code + '/images/' + prod_code + str(aux) + '.jpg'
+        try:
+           if(not os.path.exists(img_path)):
+            img.save(img_path, 'JPEG')
+        except:
+           print('Error on save img: ', img_name)
+           print('Error details: ', prd_data)
+        aux = aux + 1
+        
 
-    index = index + 1
-
-    with open ('produtos_PL', 'a', newline='', encoding='UTF-8') as f:
-       data = prod_name + ';' + prod_code + ';' + prod_price + ';' + str(prod_estoque) + ';' + str(lin) + '\n'
-       print(data)
-       f.write(data)
-    
+    index = index + 50
