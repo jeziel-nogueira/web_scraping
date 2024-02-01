b4230d8abd1d1ed6f0d966780d073953f628d287
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2024-01-31 22:30:00 -0300

    adicionado coleta SKU do produto

--- webscrap/scraper.py
+++ webscrap/scraper.py
@@ -39,13 +39,13 @@ for i in range(1, int(uls[ultima_pagina_index])+1):
 #percorrer lista de links de produtos e coletar dados
 index = 380
 for lin in lista_Links:
-    url_pagina = lin #lista_Links[index]
+    url_pagina = lista_Links[index]
     site = requests.get(url_pagina, headers=headers)
     soup = BeautifulSoup(site.content, 'html.parser')
 
 
     prod_name = soup.find('h1').get_text().strip()
-    
+    prod_code = soup.find(attrs={'itemprop':'sku'}).get_text().strip()
     prod_price = 0
     prod_estoque = 0
     
@@ -61,14 +61,15 @@ for lin in lista_Links:
     for tag in soup():
       if 'data-imagem-grande' in tag.attrs:
         image_url = tag.get("data-imagem-grande")
+        #img.save('/absolute/path/to/myphoto.jpg', 'JPEG')
         img = Image.open(requests.get(image_url, stream = True).raw)
         img_name = tag.get("title") + '.jpg'
         img.save(img_name, 'JPEG')
-        #print(ix)
+
     index = index + 1
 
     with open ('produtos_PL', 'a', newline='', encoding='UTF-8') as f:
-       data = prod_name + ';' + prod_price + ';' + str(prod_estoque) + ';' + str(lin) + '\n'
+       data = prod_name + ';' + prod_code + ';' + prod_price + ';' + str(prod_estoque) + ';' + str(lin) + '\n'
        print(data)
        f.write(data)
     
