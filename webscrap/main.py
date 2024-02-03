5103eab66eb05d4314be6edfd6d1ce40d3b98bce
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2024-02-03 01:18:13 -0300

    preparando interface

--- webscrap/main.py
+++ webscrap/main.py
@@ -2,14 +2,86 @@ import PySimpleGUI as sg
 import os, sys
 import pandas as pd
 from PIL import Image
-import csv
+import scraper
+import glob
 
-prod = pd.read_csv(r"C:\Users\ADM\Desktop\Shopee\app\produtos\PL-513\PL-513", sep=";", encoding="utf-8")
+root_path = 'C:/Users/ADM/Documents/Loja/'
+
+prod = pd.read_csv(root_path + "produtos/PL-513/PL-513", sep=";", encoding="utf-8")
 print('Data 0: ', prod['Data'][0], 'Link: ', prod['Link'][0])
 
 desc = ''
-with open(r'C:\Users\ADM\Desktop\Shopee\app\produtos\PL-513\PL-513_desc', encoding='UTF-8') as f:
+with open(root_path + 'produtos/PL-513/PL-513_desc', encoding='UTF-8') as f:
     desc = f.readlines()
 
 for lines in desc:
-    print(lines, '\n')
+    print(lines)
+
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+        sg.Button('load'),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
+image_viewer_column = [
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+
+window = sg.Window("Image Viewer", layout)
+while True:
+    
+
+    evento, valores = window.read()
+    if evento == "Sair" or evento == sg.WIN_CLOSED:
+        break
+    if evento == "-FOLDER-":
+        folder = valores["-FOLDER-"]
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif evento == "-FILE LIST-":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(
+                valores["-FOLDER-"], valores["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            window["-IMAGE-"].update(filename=filename)
+
+        except:
+            pass
+    if evento == 'load':
+        path = root_path + 'produtos/PL-513/images/*.jpg'
+        print(path)
+        for filename in glob.glob(path): #assuming gif
+            im=Image.open(filename)
+            print(filename)
+            # image_list.append(im)
+
+window.close()
+
--- webscrap/scraper.py
+++ webscrap/scraper.py
@@ -13,6 +13,10 @@ site = requests.get(url, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
 produtos = soup.find_all('li', class_='span3')
 
+root_path = 'C:/Users/ADM/Documents/Loja/'
+lista_Links = []
+
+
 
 # Descobrir quantas paginas de produtos na loja
 ultima_pagina = soup.find('li', class_='active')
@@ -25,101 +29,153 @@ for nextSibling in ultima_pagina.find_next_siblings():
 
 ultima_pagina_index  = len(uls) - 2
 
+def buscar_novos_prdutos():
+   #descobrir maximo de paginas de produtos
+   site = requests.get(url, headers=headers)
+   soup = BeautifulSoup(site.content, 'html.parser')
+   produtos = soup.find_all('li', class_='span3')
+   ultima_pagina = soup.find('li', class_='active')
+   uls = []
+   for nextSibling in ultima_pagina.find_next_siblings():
+      if nextSibling.name == 'h1':
+         break
+      if nextSibling.name == 'li':
+         uls.append(nextSibling.get_text().strip())
+   ultima_pagina_index  = len(uls) - 2
+
+   # Pegar link de todos os produtos aind nao salvos
+   for i in range(1, int(uls[ultima_pagina_index])+1):
+      url_pagina = f'https://www.piscaled.com.br/eletronica?pagina={i}#'
+      site = requests.get(url_pagina, headers=headers)
+      soup = BeautifulSoup(site.content, 'html.parser')
+      lista_produtos = soup.find_all('li', class_='span3')
+      index = 0
+      for prod in lista_produtos:
+         links = lista_produtos[index].find_all("a", class_='produto-sobrepor')
+         for link in links:
+            x = link.get("href")
+            lista_Links.append(x)
+         index = index + 1
+
+
 def criar_pasta(nome):
-   pasta_main = '../produtos/'
+   pasta_main = root_path + 'produtos/'
    if(not os.path.exists(pasta_main)):
       os.mkdir(pasta_main)
-   if(not os.path.exists('../produtos/{nome}/')):
+   if(not os.path.exists(pasta_main + '{nome}/')):
       try:
          os.mkdir(os.path.join(pasta_main, nome))
-         img_path = '../produtos/' + nome
+         img_path = pasta_main + nome
          os.mkdir(os.path.join(img_path, 'images'))
       except:
          print('Folder alreade exist')
 
 def save_data(data_, prod_path, desc, prod_path_desc):
       cabecalho = 'Data;Nome;SKU;Preço;Estoque;Link' + '\n'
-      if(not os.path.exists(prod_path_desc)):
-         with open (prod_path_desc, 'a', newline='', encoding='UTF-8') as f:
-            f.write(desc)
+      flag_new = 'true'
+      flag_path = prod_path + '_flag'
+      flag_title = 'new'
+         
       if(not os.path.exists(prod_path)):
          with open (prod_path, 'a', newline='', encoding='UTF-8') as f:
             f.write(cabecalho)
             f.write(data_)
+         with open (prod_path_desc, 'a', newline='', encoding='UTF-8') as f:
+            f.write(desc)
+         with open (flag_path, 'a', newline='', encoding='UTF-8') as f:
+            f.write('new' + '\n')
+            f.write('true')
       else:
          with open (prod_path, 'a', newline='', encoding='UTF-8') as f:
             f.write(data_)
    
-# Pegar link de todos os produtos 
-lista_Links = []
-for i in range(1, int(uls[ultima_pagina_index])+1):
-    url_pagina = f'https://www.piscaled.com.br/eletronica?pagina={i}#'
-    site = requests.get(url_pagina, headers=headers)
-    soup = BeautifulSoup(site.content, 'html.parser')
-    lista_produtos = soup.find_all('li', class_='span3')
-    index = 0
-    for prod in lista_produtos:
-        links = lista_produtos[index].find_all("a", class_='produto-sobrepor')
-        for link in links:
-          x = link.get("href")
-          lista_Links.append(x)
-        index = index + 1
-
-#percorrer lista de links de produtos e coletar dados
-index = 0
-for lin in lista_Links:
-    url_pagina = lista_Links[index]
-    site = requests.get(url_pagina, headers=headers)
-    soup = BeautifulSoup(site.content, 'html.parser')
-
-
-    prod_name = soup.find('h1').get_text().strip()
-    prod_code = soup.find(attrs={'itemprop':'sku'}).get_text().strip()
-    prod_price = 0
-    prod_estoque = 0
-    prod_desc = 'ND'
-
-    try:
-       prod_desc = soup.find(id='descricao').get_text().strip()
-    except:
-       prod_desc = 'ND'
-    
-    prod_folder = prod_code + '/'
-    criar_pasta(prod_folder)
-    try:
-       estoque = soup.find(class_='qtde_estoque').get_text().strip()
-       prod_estoque = estoque
-       price = soup.find(class_='preco-promocional cor-principal titulo').get_text().strip()
-       prod_price = price[3:]
-    except:
-       prod_price = 'nd'
-       prod_estoque = 0
-
-    dt_string = today.strftime("%d/%m/%Y %H:%M:%S")
-    prd_data = dt_string + ';' + prod_name + ';' + prod_code + ';' + prod_price + ';' + str(prod_estoque) + ';' + lista_Links[index] + '\n'
-    prod_path = '../produtos/' + prod_code + '/' + prod_code
-    _desc = prod_code + '_desc'
-    prod_path_desc = '../produtos/' + prod_code + '/' + _desc
-    
-    save_data(prd_data, prod_path, prod_desc, prod_path_desc)
-
-    aux = 1
-    for tag in soup():
+def prod_images(link_, prod_code):
+   site = requests.get(link_, headers=headers)
+   soup = BeautifulSoup(site.content, 'html.parser')
+   img_Index = 1
+   for tag in soup():
       if 'data-imagem-grande' in tag.attrs:
         image_url = tag.get("data-imagem-grande")
         img = Image.open(requests.get(image_url, stream = True).raw)
         img_name = tag.get("title") + '.jpg'
-        img_path = '../produtos/' + prod_code + '/images/' + prod_code + str(aux) + '.jpg'
+        img_path = root_path + 'produtos/' + prod_code + '/images/' + prod_code + str(img_Index) + '.jpg'
         try:
            if(not os.path.exists(img_path)):
             img.save(img_path, 'JPEG')
         except:
            print('Error on save img: ', img_name)
            print('Error details: ', prd_data)
-        aux = aux + 1
-        
+        img_Index = img_Index + 1
 
-    index = index + 50
+def get_prod_estoque(link_):
+   val = 0
+   site = requests.get(link_, headers=headers)
+   soup = BeautifulSoup(site.content, 'html.parser')
+   try:
+      val = soup.find(class_='qtde_estoque').get_text().strip()
+   except:
+      val = 0
+   return val
 
-def print_text(text):
-   print(text)
+def get_prod_preco(link_):
+   site = requests.get(link_, headers=headers)
+   soup = BeautifulSoup(site.content, 'html.parser')
+
+   val = 0
+   try:
+      price = soup.find(class_='preco-promocional cor-principal titulo').get_text().strip()
+      val = price[3:]
+   except:
+      val = 'nd'
+   return val
+
+
+
+#percorrer lista de links de produtos e coletar dados
+
+def load_data():
+   buscar_novos_prdutos()
+   index = 0
+   for lin in lista_Links:
+      url_pagina = lista_Links[index]
+      site = requests.get(url_pagina, headers=headers)
+      soup = BeautifulSoup(site.content, 'html.parser')
+
+
+      prod_name = soup.find('h1').get_text().strip()
+      prod_code = soup.find(attrs={'itemprop':'sku'}).get_text().strip()
+      prod_price = get_prod_preco(lista_Links[index])
+      prod_estoque = get_prod_estoque(lista_Links[index])
+      prod_desc = 'ND'
+
+      try:
+         prod_desc = soup.find(id='descricao').get_text().strip()
+      except:
+         prod_desc = 'ND'
+      
+      prod_folder_name = prod_code + '/'
+      criar_pasta(prod_folder_name)
+
+      #salvar dados do produto
+      dt_string = today.strftime("%d/%m/%Y %H:%M:%S")
+      prd_data = dt_string + ';' + prod_name + ';' + prod_code + ';' + prod_price + ';' + str(prod_estoque) + ';' + lista_Links[index] + '\n'
+      prod_path = root_path + 'produtos/' + prod_code + '/' + prod_code
+      _desc = prod_code + '_desc'
+      prod_path_desc = root_path + 'produtos/' + prod_code + '/' + _desc
+      
+      save_data(prd_data, prod_path, prod_desc, prod_path_desc)
+
+      # salvar imagens do produto
+      prod_images(lista_Links[index], prod_code)
+      index = index + 50
+      if(index >= 350):
+         break
+
+
+
+
+
+
+# Para cada novo prduto criar um arquivo txt flag com booleana para new=true, para poder saber a diferença entre antigos e novos
+# Funçao para verificar mudanças em estoque e preço
+# Salvar apenas registro de estoque diario, nao salvar informaçoes repetidas como descricao, nome, etc
